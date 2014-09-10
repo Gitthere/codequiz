236ca936522b914cbbbd812959795edecad545a2
Gitthere <ichi4g@gmail.com>
2014-09-09 14:18:15 -1000

    testing ques generation

--- client/views/index.js
+++ client/views/index.js
@@ -12,3 +12,20 @@ Session.setDefault("counter", 0);
       Session.set("counter", Session.get("counter") + 1);
     }
   });
+
+//set up collection of player names 
+
+
+
+//set up collection of scores
+
+
+//set up collection of questions for random generation
+Template.questionAndAnswerContainer.question = function() {
+  return Session.get("currentQuestion");
+};
+
+setInterval(function() {
+  console.log('test');
+  Session.set("currentQuestion", Date.now);
+}, 1000);
