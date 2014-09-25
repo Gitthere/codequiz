55e0c5e3afdddf83f9b63e7f4affd72cb8df4299
Gitthere <ichi4g@gmail.com>
2014-09-25 12:10:04 -1000

    able to seed data and have questions populate

--- server/server.js
+++ server/server.js
@@ -1,50 +1,107 @@
 Meteor.startup(function () {
     // code to run on server at startup
-    if (Questions.find({}).fetch().length === 0) {
-      //seed data
-      Questions.insert({
-        body : "In the MEAN stack, what does the 'A' stand for?",
-        answers : [
-          {
-            answer: "Apache",
-            correct: false
-          },
-          {
-            answer: "Asynchronous",
-            correct: false
-          },
-          {
-            answer: "Angular",
-            correct: true
-          },
-          {
-            answer: "Adobe",
-            correct: false
-          },
-        ]
-      });//******* SYNTAX FOR INSERTING ADDTL QUES?
+  Questions.remove({});
+    //seed data
+    Questions.insert({
+      body : "In the MEAN stack, what does the 'A' stand for?",
+      answers : [
+        {
+          answer: "Apache",
+          correct: false
+        },
+        {
+          answer: "Asynchronous",
+          correct: false
+        },
+        {
+          answer: "Angular",
+          correct: true
+        },
+        {
+          answer: "Adobe",
+          correct: false
+        },
+      ]
+    });//******* SYNTAX FOR INSERTING ADDTL QUES?
 
-      Questions.insert({
-        body : "In the LAMP stack, what does the 'A' stand for?",
-        answers : [
-          {
-            answer: "ALSO",
-            correct: false
-          },
-          {
-            answer: "APPLE",
-            correct: false
-          },
-          {
-            answer: "AUTO",
-            correct: true
-          },
-          {
-            answer: "ANT",
-            correct: false
-          },
-        ]
-      });
+    Questions.insert({
+     body : "sample ques 2",
+     answers : [
+     {
+     answer: "snow",
+     correct: false
+     },
+     {
+     answer: "rain",
+     correct: true
+     },
+    ]
+    });
 
-    }
-});
+    Questions.insert({
+      body : "In the LAMP stack, what does the 'A' stand 3?",
+      answers : [
+        {
+          answer: "ALSO",
+          correct: false
+        },
+        {
+          answer: "APPLE",
+          correct: false
+        },
+        {
+          answer: "AUTO",
+          correct: true
+        },
+        {
+          answer: "ANT",
+          correct: false
+        },
+      ]
+    });
+
+    Questions.insert({
+      body : "In the LAMP stack, what does the 'A' stand 4?",
+      answers : [
+        {
+          answer: "ALSO",
+          correct: false
+        },
+        {
+          answer: "APPLE",
+          correct: false
+        },
+        {
+          answer: "AUTO",
+          correct: true
+        },
+        {
+          answer: "ANT",
+          correct: false
+        },
+      ]
+    });
+
+    Questions.insert({
+      body : "In the LAMP stack, what does the 'A' stand 5?",
+      answers : [
+        {
+          answer: "ALSO",
+          correct: false
+        },
+        {
+          answer: "APPLE",
+          correct: false
+        },
+        {
+          answer: "AUTO",
+          correct: true
+        },
+        {
+          answer: "ANT",
+          correct: false
+        },
+      ]
+    });
+  }
+);
